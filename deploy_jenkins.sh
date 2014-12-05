5e2f9bafab4a4d267d358beab481ffbff66faf88
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2014-12-05 09:30:43 +0100

    Install jekyll

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -1,9 +1,13 @@
-#! /bin/sh
+#!/usr/bin/env bash
+set -e # halt script on error
+
 
 echo "Ready to reploy updated version of voot to openvoot.org"
 
 . ~/cf-login.sh
 cf target -o system -s prod
+
+gem install jekyll
 
 npm install
 node_modules/grunt-cli/bin/grunt publish
